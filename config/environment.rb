9884de711d3c0fbbd9d2fba46ddcd08c91a48962
Balaji Damodaran <damodaran.balaji@gmail.com>
2011-02-18 20:15:20 +0530

    fdfd

--- config/environment.rb
+++ config/environment.rb
@@ -34,6 +34,7 @@ Rails::Initializer.run do |config|
   # Run "rake -D time" for a list of tasks for finding time zone names.
   config.time_zone = 'Chennai'
   config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
+  config.action_controller.session_store = :active_record_store
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
