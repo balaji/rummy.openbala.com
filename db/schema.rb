332070679f2786291429a17721d23079026e6c1a
Balaji Damodaran <damodaran.balaji@gmail.com>
2011-02-19 08:16:28 +0530

    datetime permanent fix

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110218144722) do
+ActiveRecord::Schema.define(:version => 20110219024443) do
 
   create_table "authorizations", :force => true do |t|
     t.string   "provider"
