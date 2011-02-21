c792dfc0492aa01b5bbb0ab7a1b75e828af091f7
Balaji Damodaran <damodaran.balaji@gmail.com>
2011-02-21 23:03:08 +0530

    revert

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,7 +1,7 @@
 class ScheduleController < ApplicationController
   before_filter :authenticate
   layout "standard"
-  caches_action :index
+#  caches_action :index
 
   def index
     @schedule = Match.all.sort_by { |s| s.date }
