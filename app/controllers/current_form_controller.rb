52d98c7f864867b38c1b66ce1827c8fe27c17629
openbala <damodaran.balaji@gmail.com>
2011-02-25 21:22:50 +0530

    bang v ire

--- app/controllers/current_form_controller.rb
+++ app/controllers/current_form_controller.rb
@@ -3,11 +3,11 @@ class CurrentFormController < ApplicationController
   layout "standard"
 
   def index
-    match_id = Match.find(:first, :conditions => ['Status = ?', 'finished'], :order => 'id desc').id
-    user = CurrentForm.find_by_user_id_and_match_id(self.current_user.id, match_id)
+    #match_id = Match.find(:first, :conditions => ['Status = ?', 'finished'], :order => 'id desc').id
+    user = CurrentForm.find_by_user_id_and_match_id(self.current_user.id, 22)
     page = params[:page] ? params[:page] : (!user.nil? ? (user.rank / 10) + 1 : 1)
     @current_form = CurrentForm.paginate(:all, 
-                                         :conditions=> ['match_id = ?', match_id], 
+                                         :conditions=> ['match_id = ?', 22], 
                                          :page => page, 
                                          :per_page => User.per_page)
   end
