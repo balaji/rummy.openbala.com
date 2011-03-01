0a1bc3e30f7c9d74d5705bbec48b1260f25f72a0
openbala <damodaran.balaji@gmail.com>
2011-03-01 08:42:11 +0530

    deleted

--- config/environment.rb
+++ config/environment.rb
@@ -33,9 +33,7 @@ Rails::Initializer.run do |config|
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
   config.time_zone = 'Chennai'
-  config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
   config.action_controller.session_store = :active_record_store
-  config.gem 'dalli'
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
