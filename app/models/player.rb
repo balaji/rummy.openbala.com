fda7b7de404e55fb2855884d6489be29f3924dc4
openbala <damodaran.balaji@gmail.com>
2011-03-02 15:14:56 +0530

    facelift

--- app/models/player.rb
+++ app/models/player.rb
@@ -74,11 +74,7 @@ class Player < ActiveRecord::Base
     end
   end
 
-  def self.played_match_country_one(match_id)
-    Player.find_by_sql(["select * from players p where p.id in (select player_id from batting_score_cards b where b.match_id = ?) and p.country_id = (select m.country_one_id from matches m where m.id = ?)", match_id, match_id])
-  end
-
-  def self.played_match_country_two(match_id)
-    Player.find_by_sql(["select * from players p where p.id in (select player_id from batting_score_cards b where b.match_id = ?) and p.country_id = (select m.country_two_id from matches m where m.id = ?)", match_id, match_id])
+  def self.played_match(match_id)
+    Player.find_by_sql(["select * from players p where p.id in (select player_id from batting_score_cards b where b.match_id = ?) and (p.country_id = (select m.country_one_id from matches m where m.id = ?) or p.country_id = (select m.country_two_id from matches m where m.id = ?))", match_id, match_id, match_id])
   end
 end
