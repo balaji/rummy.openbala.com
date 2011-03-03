5377a1f0f9cfca57b8ad7f5259bc0283fc6d0750
openbala <damodaran.balaji@gmail.com>
2011-03-03 23:34:36 +0530

    logs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,6 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
-
-  # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  helper :all
+  protect_from_forgery
   protected
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
--- lib/tasks/cron.rake
+++ lib/tasks/cron.rake
@@ -2,6 +2,7 @@ task :cron => :environment do
   if Time.now.in_time_zone('Chennai').hour == 0
     User.all.each do |user|
       if Rails.cache.exists? "#{user.id}_friends"
+        puts "removing cache of #{user.name}"
         Rails.cache.delete "#{user.id}_friends"
       end
     end
