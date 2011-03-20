e4e180e26e6fff99407e318d64f0414fb528f4cc
Balaji Damodaran <damodaran.balaji@gmail.com>
2011-03-20 22:26:43 +0530

    replacements

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110320162444) do
+ActiveRecord::Schema.define(:version => 20110320164921) do
 
   create_table "authorizations", :force => true do |t|
     t.string   "provider"
